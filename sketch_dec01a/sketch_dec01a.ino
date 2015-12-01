70603ba73e4de11a186c00841057eae5ff08ab34
1000000000 <lthyde@gmail.com>
2015-12-01 14:36:03 -0500

    two leds

--- sketch_dec01a/sketch_dec01a.ino
+++ sketch_dec01a/sketch_dec01a.ino
@@ -1,13 +1,17 @@
-int led=8;
+int led1=8;
+int led2=9;
 void setup() {
   // put your setup code here, to run once:
-pinMode(led, OUTPUT);
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(led, HIGH);
+digitalWrite(led1, HIGH);
+digitalWrite(led2, HIGH);
 delay(500);
-digitalWrite(led, LOW);
+digitalWrite(led1, LOW);
+digitalWrite(led2, LOW);
 delay(500);
 }
